9eaaac2fc12f6dafa75fb8e145e64f2741233d93
Zeke Flom <zekeage@gmail.com>
2015-06-29 00:51:06 -0700

    new query with mod

--- server.js
+++ server.js
@@ -134,7 +134,8 @@ var alphabet = function() {
             var fid = req.query['fid'];
             console.log(fid);
             //var data = {"Data":""};
-            connection.query("SELECT fact FROM facts where id = mod(" + fid + ",(select count(*) FROM facts) ))",function(err, rows, fields){
+            
+            connection.query("select fact from facts where id = (1+mod(" + fid + "-1, (SELECT count(*) from facts)));",function(err, rows, fields){
             //connection.query("SELECT fact FROM facts as r1 JOIN \
             //(SELECT CEIL(RAND() * (SELECT MAX(id) FROM random)) AS id) AS r2 \
             //WHERE r1.id >= r2.id ORDER BY r1.id ASC LIMIT 1", 
